793d9ff008828bd62acb7b037a5de3a5b554c0c7
Ayush <ayush.jan10@gmail.com>
2019-12-23 13:25:40 +0530

    Project_dir added

--- DBPortal/DBPortal/settings.py
+++ DBPortal/DBPortal/settings.py
@@ -122,7 +122,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     STATIC_DIR,
