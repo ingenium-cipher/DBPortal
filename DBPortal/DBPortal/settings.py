b11fa1c103bde8dd091995db364b47d923076ac0
Ayush <ayush.jan10@gmail.com>
2019-12-23 12:14:30 +0530

    django-heroku installed

--- DBPortal/DBPortal/settings.py
+++ DBPortal/DBPortal/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -140,3 +141,5 @@ EMAIL_HOST_PASSWORD = 'ayush@123'
 EMAIL_PORT = 587
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
+django_heroku.settings(locals())
